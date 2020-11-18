724a7c94569d09686926d8eca40e5fad372fbf5d
luan1789 <nguyenvanluan1789@gmail.com>
2020-11-18 08:00:00 +0700

    optimize the getF() by using recursion

--- src/luan/util/MathUtility.java
+++ src/luan/util/MathUtility.java
@@ -16,6 +16,22 @@ public class MathUtility {
     // chung toan la static, thu vien dung chung
     public static double PI = 3.1415;
 
+//    public static long getPactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("N must be between 0..20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i; 
+//        }
+//        return result;
+//    }
+    
+    //viet de quy cho hoanh trang
+    //n!=n*(n-1)!
     public static long getPactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("N must be between 0..20");
@@ -23,11 +39,11 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i; 
-        }
-        return result;
+        //n>1 & n<=20
+        return n*getPactorial(n-1);
     }
+    //minh hoa khai niem refactoring -toi uu chinh sua lai code cho tot hon
+    //minh hoa khai niem regression test-test lai nhung j da test de check coi
+    //code con sach khong sau khi sua code
     
 }
